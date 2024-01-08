f38b000db47882197521c8d2938592072840f79e
nghia0111 <20521659@gm.uit.edu.vn>
2024-01-08 16:47:48 +0700

    update profile api

--- src/api/user/controllers/userController.ts
+++ src/api/user/controllers/userController.ts
@@ -6,6 +6,7 @@ import { BadRequestException, ForbiddenException, NotFoundException } from "@nes
 import { makeToken } from "../../../../src/common/helper/token";
 import { hashPass } from "../../../../src/common/helper/hashPass";
 import { firebaseAdmin } from "../../../config/firebase";
+//import * as multer from 'multer';
 const Joi = require("joi");
 
 const register = async (req: Request, res: Response) => {
@@ -80,7 +81,63 @@ const login = async (req: Request, res: Response) => {
   }
 }
 
+const getProfile = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userData.id;
+    const userService = new UserService();
+
+    const { password, ...user } = await userService.getOne({where: {id: userId}}); 
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ detail: e.message });
+  }
+};
+
+const updateProfile = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userData.id;
+
+    const schema = Joi.object({
+      fullName: Joi.string().required(),
+      avatar: Joi.string().required(),
+      dob: Joi.date().optional(),
+      gender: Joi.string()
+        .valid(...Object.values(Gender))
+        .required(),
+      phone: Joi.string()
+        .pattern(/^[0-9]{10}$/)
+        .optional(),
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) return res.status(400).json({ detai: error.message });
+
+    const userService = new UserService();
+
+    const user = await userService.getOne({
+      where: { id: userId },
+    });
+
+    const newUser = userService.create({...user, ...value});
+    const {password, ...userInfor} = await userService.save(newUser);
+
+
+    return res.status(200).json(userInfor);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ detail: e.message });
+  }
+};
+
+// const upload = async (req: Request, res: Response) => {
+ 
+// };
+
 export const userController = {
   register,
   login,
+  getProfile,
+  updateProfile
 };
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -3,9 +3,12 @@ const router = express.Router();
 import { userController } from './controllers/userController';
 import Auth from '../../common/helper/auth';
 
-const auth = new Auth();
+const authInstance = new Auth();
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get("/profile", authInstance.auth, userController.getProfile);
+router.put("/update-profile", authInstance.auth, userController.updateProfile);
+//router.post("/upload", auth.auth, userController.upload);
 
 module.exports = router;
